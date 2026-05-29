aa330b9e3b1379d17bacd32fb3bf20e462476e30
LIN <404@reach.com>
2026-05-29 10:52:21 +0800

    chapter7: fetching data, 首页的发票、卡片、chart组件

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,7 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px] font-bold">Acme</p>
+      LOGO
     </div>
   );
 }
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex items-end justify-start rounded-md bg-blue-600 p-4"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
